f0ba25c9cec7613d83667ac68667b5d9200eb9dc
Kavimani-S <112235556+Kavimani-S@users.noreply.github.com>
2024-05-14 17:13:37 +0530

    Update test_calc.py

--- check/testd/test_calc.py
+++ check/testd/test_calc.py
@@ -1,14 +1,16 @@
 # test.py
 import sys
 import os
-import pytest
+import unittest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from dummytest.main import Calculator
-def test_add(self):
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
         calc = Calculator()
         self.assertEqual(calc.add(1, 2), 3)
         self.assertEqual(calc.add(-1, 1), 0)
         self.assertEqual(calc.add(0, 0), 0)
 
 if __name__ == "__main__":
-    pytest.main()
+    unittest.main()
