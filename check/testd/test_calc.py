2eb0cec22c6014983ded94a8bc7d476d985740ce
Kavimani-S <112235556+Kavimani-S@users.noreply.github.com>
2024-05-15 09:17:55 +0530

    Update test_calc.py

--- check/testd/test_calc.py
+++ check/testd/test_calc.py
@@ -1,16 +1,22 @@
-# test.py
+# test_calculator.py
 import sys
 import os
-import unittest
+import pytest
+
+
+
+# Add the parent directory to the sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from dummytest.main import Calculator
+def test_add():
+    calc = Calculator()
+    assert calc.add(1, 2) == 3
+    assert calc.add(-1, 1) == 0
+    assert calc.add(0, 0) == 0
 
-class TestCalculator(unittest.TestCase):
-    def test_add(self):
-        calc = Calculator()
-        self.assertEqual(calc.add(1, 2), 3)
-        self.assertEqual(calc.add(-1, 1), 0)
-        self.assertEqual(calc.add(0, 0), 0)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_subtract():
+    calc = Calculator()
+    assert calc.subtract(3, 1) == 2
+    assert calc.subtract(0, 0) == 0
+    assert calc.subtract(-1, -1) == 0
