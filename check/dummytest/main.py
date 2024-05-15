287e911033b89a72a46057c91d2b72716197b0fe
Kavimani-S <112235556+Kavimani-S@users.noreply.github.com>
2024-05-15 09:18:28 +0530

    Update main.py

--- check/dummytest/main.py
+++ check/dummytest/main.py
@@ -1,10 +1,9 @@
 # main.py
-def add(x, y):
-    return x + y
-
 class Calculator:
     def __init__(self):
         pass
-
     def add(self, x, y):
         return x + y
+
+    def subtract(self, x, y):
+        return x - y
